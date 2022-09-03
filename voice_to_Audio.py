6a9a5c10f115fef7bd426eca4146cc741dbfad84
GayathriSS-19 <cp.gayathrisaisree@gmail.com>
2022-09-03 18:14:16 +0530

    Final VirtualAssistant code
    Added web scrapping,sending email and opening google functions

--- voice_to_Audio.py
+++ voice_to_Audio.py
@@ -6,6 +6,11 @@ from gtts import gTTS
 import os
 import playsound
 import time
+import re         #to search while opening a web page
+import requests     #to connect to a webpage
+import webbrowser   #to open a webpage
+import bs4
+import smtplib      #to send an email-simple mail transfer protocol
 
 
 #to make sure it listen
@@ -42,10 +47,64 @@ def voice_assistant(data):
     if "time" in data:
         listening=True
         respond(time.ctime())
-time.sleep(2)
+    if "open google" in data.casefold():
+        listening=True
+        # reg_ex=re.search('open google(.*)',data)
+        url="https://www.google.com/"
+        # if reg_ex:
+        #     sub=reg_ex.group(1)
+        #     url=url+'r/'
+        webbrowser.open(url)
+        respond("Done")
+    #email gives error as less secure is no longer supported
+    # if "email" in data:
+    #     listening=True
+    #     respond("Whom should I send the mail to")
+    #     to=listen()
+    #     edict={'hello':"mrygayathri@gmail.com",'just':"gayathrisaisreepolamarasetti@gmail.com"}
+    #     toaddr=edict[to]
+    #     respond("What is the subject?")
+    #     subject=listen()
+    #     respond("What should I tell the person?")
+    #     message=listen()
+    #     content='Subject:{}\n\n{}'.format(subject,message)
+
+    #     #init gmail SMTP
+    #     mail = smtplib.SMTP('smtp.gmail.com',587)
+    #     #identify the server
+    #     mail.ehlo()
+    #     mail.starttls()
+    #     #login
+    #     mail.login('gayathrisaisreepolamarasetti@gmail.com','*Saisri6515')
+    #     mail.sendmail('gayathrisaisreepolamarasetti@gmail.com',toaddr,content)
+    #     respond("Email sent")
+
+    #webscraping
+    if "wiki" in data.casefold():
+        listening=True
+        url="https://en.wikipedia.org/wiki/"
+        respond("What should I Search")
+        query =listen()
+        response=requests.get(url+query)
+        if response is not None:
+            html=bs4.BeautifulSoup(response.text,'html.parser')
+            paragraphs=html.select('p')
+            intro=[i.text for i in paragraphs]
+            halo=' '.join(intro)
+            respond(halo[:200])
+    if "stop" in data:
+        listening=False
+        print("Listening Stopped")
+        respond("See you Gayathri")
+    try:
+        return listening
+    except UnboundLocalError:
+        print("Timed out")
+
+#time.sleep(2)
 respond("Hey Gayathri How are you")
 listening =True
-if listening==True:
+while listening==True:
     data=listen()
     listening =voice_assistant(data)
 
